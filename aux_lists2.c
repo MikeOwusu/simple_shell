95bb9d94d9a9bdf7f0faffe2a2607baae7da9e6e
MikeOwusu <125754182+MikeOwusu@users.noreply.github.com>
2023-05-27 22:15:32 +0000

    aux_lists2.c
    simple shell

--- aux_lists2.c
+++ aux_lists2.c
@@ -1,76 +1,26 @@
 #include "main.h"
 
 /**
- * add_sep_node_end - adds a separator found at the end
- * of a sep_list.
- * @head: head of linked list
- * @sep: separator found (; | &)
- * Return: address of the head
- */
-sep_list *add_sep_node_end(sep_list **head, char sep)
-{
-	sep_list *new, *temp;
-
-	new = malloc(sizeof(sep_list));
-	if (new == NULL)
-		return (NULL);
-
-	new->separator = sep;
-	new->next = NULL;
-	temp = *head;
-
-	if (temp == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
-
-	return (*head);
-}
-
-/**
- * free_sep_list - frees a sep_list
+ * add_rvar_node - adds a variable at the end
+ * of a r_var list.
  * @head: head of the linked list.
- * Return: no return.
- */
-void free_sep_list(sep_list **head)
-{
-	sep_list *temp;
-	sep_list *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * add_line_node_end - adds a command line at the end
- * of a line_list.
- * @head: head of the linked list.
- * @line: command line.
+ * @lvar: length of variable
+ * @val: value of variable.
+ * @lval: length of the value.
  * Return: address of the head.
  */
-line_list *add_line_node_end(line_list **head, char *line)
+r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
 {
-	line_list *new, *temp;
+	r_var *new, *temp;
 
-	new = malloc(sizeof(line_list));
+	new = malloc(sizeof(r_var));
 	if (new == NULL)
 		return (NULL);
 
-	new->line = line;
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
+
 	new->next = NULL;
 	temp = *head;
 
@@ -89,14 +39,14 @@ line_list *add_line_node_end(line_list **head, char *line)
 }
 
 /**
- * free_line_list - frees a line_list
- * @head: head of inked list.
+ * free_rvar_list - frees a r_var list
+ * @head: head of the linked list.
  * Return: no return.
  */
-void free_line_list(line_list **head)
+void free_rvar_list(r_var **head)
 {
-	line_list *temp;
-	line_list *curr;
+	r_var *temp;
+	r_var *curr;
 
 	if (head != NULL)
 	{
@@ -110,3 +60,24 @@ void free_line_list(line_list **head)
 	}
 }
 
+/**
+ * free_rvar_list - frees a r_var list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
