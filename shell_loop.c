5c9842676254f45eeee1115362de0c23b9141bde
MikeOwusu <owusukwekumichael@gmail.com>
2023-07-26 18:30:26 -0700

    test run

--- shell_loop.c
+++ shell_loop.c
@@ -4,7 +4,8 @@
  * hsh - main shell loop
  * @info: the parameter & return info struct
  * @av: the argument vector from main()
- * Return: 0 for success, 1 for error, or error code
+ *
+ * Return: 0 on success, 1 on error, or error code
  */
 int hsh(info_t *info, char **av)
 {
@@ -45,9 +46,10 @@ int hsh(info_t *info, char **av)
 /**
  * find_builtin - finds a builtin command
  * @info: the parameter & return info struct
+ *
  * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully
- * 	1 if builtin found but not successful
+ * 	0 if builtin executed successfully,
+ * 	1 if builtin found but not successful,
  * 	2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
@@ -72,13 +74,13 @@ int find_builtin(info_t *info)
 			built_in_ret = builtintbl[i].func(info);
 			break;
 		}
-	
 	return (built_in_ret);
 }
 
 /**
  * find_cmd - finds a command in PATH
  * @info: the parameter & return info struct
+ *
  * Return: void
  */
 void find_cmd(info_t *info)
@@ -118,8 +120,9 @@ void find_cmd(info_t *info)
 }
 
 /**
- * fork_cmd - forks an exec thread to run cmd
+ * fork_cmd - forks a an exec thread to run cmd
  * @info: the parameter & return info struct
+ *
  * Return: void
  */
 void fork_cmd(info_t *info)
@@ -129,7 +132,7 @@ void fork_cmd(info_t *info)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		/* TODO: INPUT ERROR FUNCTION */
+		/* TODO: PUT ERROR FUNCTION */
 		perror("Error:");
 		return;
 	}
@@ -142,7 +145,7 @@ void fork_cmd(info_t *info)
 				exit(126);
 			exit(1);
 		}
-		/* TODO:INPUT ERROR FUNCTION */
+		/* TODO: PUT ERROR FUNCTION */
 	}
 	else
 	{
